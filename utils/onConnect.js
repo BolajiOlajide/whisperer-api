94bd9154585043076d71074a1894fe24ce9a619a
Bolaji Olajide <cooproton@gmail.com>
2020-10-18 23:44:40 +0100

    feat: fix whisperAdded bug

--- utils/onConnect.js
+++ utils/onConnect.js
@@ -1,3 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config = require('lazy-config');
+
+const userLoader = require('../dataloaders/user.loader');
+const whisperLoader = require('../dataloaders/whisper.loader');
+
+
 module.exports = (connectionParams, webSocket, context) => {
-  console.log('connected')
+  const token = connectionParams['Authorization'] || '';
+  let user;
+
+  if (token) {
+    try {
+      user = jwt.verify(token, config.authentication.secret);
+    } catch {
+      user = null;
+    }
+  }
+
+  return {
+    user,
+    userLoader,
+    whisperLoader,
+  }
 };
